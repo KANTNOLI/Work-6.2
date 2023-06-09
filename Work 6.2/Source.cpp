6ecd9d5a288d8e2e889f2710af0d28444ec6ce5e
KANTNOLI <zahargysev02@gmail.com>
2023-06-09 19:42:18 +0300

    delere class, ready

--- Work 6.2/Source.cpp
+++ Work 6.2/Source.cpp
@@ -8,16 +8,59 @@ using namespace std;
 //вылета.Вывести все номера рейсов и время вылета самолета для заданного пункта
 //назначения.
 
-class Airport {
-public:
-	int flyNumber;
-	string destination;
-
-
-};
-
 int main() {
+	setlocale(LC_ALL, "RUS");
+	ifstream fin;
+	bool flag = true;
+	string inDestin, time;
+	int number;
+	
+	//Ввод данных
+	ofstream fout;
+	fout.open("Airport.txt");
+	fout << "USA       12.00  1\n";
+	fout << "Brazil    13.00  2\n";
+	fout << "Canada    14.00  3\n";
+	fout << "China     15.00  4\n";
+	fout << "Colombia  16.00  5\n";
+	fout << "Cuba      17.00  6\n";
+	fout << "Egypt     18.00  7\n";
+	fout << "Germany   19.00  8\n";
+	fout << "Iran      20.00  9\n";
+	fout << "Japan     21.00  10\n";
+	fout.close();
+	
 
+
+	cout << "USA Brazil Canada China Colombia Cuba Egypt Germany Iran Japan\nВыберите пункт назначения: ";
+	cin >> inDestin;
+
+	fin.open("Airport.txt");
+
+	if (!fin.is_open()) {
+		cout << "Ошибка! Файл не открыт." << endl;
+	}
+	else {
+		while (flag) {
+			string destination;
+
+			fin >> destination;
+			if (destination == inDestin) {
+				fin >> time;
+				fin >> number;
+
+				break;
+			}
+
+			destination = "";
+			flag = !fin.eof();
+		}
+
+	}
+
+	fin.close();
+
+	cout << "Страна: " << inDestin << "\nНомер рейса: " << number << "\nВремя вылета сегодня: " << time << endl;
 
 	return 0;
 }
